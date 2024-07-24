024fc78790832f6a92a947a93475c463d21803c0
DINESH <dineskj@integramicro.co.in>
2024-07-24 22:02:38 +0530

    Commit after merging about me section2

--- src/components/about/Panel.jsx
+++ src/components/about/Panel.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import './About.css';
-
+import myimage from '../../assets/myImage.jpeg'
 
 function Panel() {
     return (
         <div className="rounded-2xl shadow-xl ">
-            <img className="border-b-orange-100 rounded-3xl" src="src\assets\myImage.jpeg" alt="Display image" style={{ boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)' }} />
+            <img className="border-b-orange-100 rounded-3xl" src={myimage} alt="Display image" style={{ boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)' }} />
         </div>
     );
 }
